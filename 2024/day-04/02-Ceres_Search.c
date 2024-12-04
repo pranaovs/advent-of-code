6205bb03cf737bba2fb567e63335cdf6d9ccd271
Pranaov <148376495+pranaovs@users.noreply.github.com>
2024-12-05 05:01:59 +0530

    2024 Day-04 Part-2: Ceres Search - literal X-MAS

---
+++ 2024/day-04/02-Ceres_Search.c
@@ -0,0 +1,93 @@
+// Advent of code 2024 Day-04 Part-01
+
+// Ceres Search
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_LINE_LENGTH 2048
+
+int main(int argc, char *argv[]) {
+
+  FILE *f;
+
+  // Check for input file
+  if (argc < 2) {
+    fprintf(stderr, "Usage: %s <input file>\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  // Open file
+  if ((f = fopen(argv[1], "r")) == NULL) {
+    fprintf(stderr, "Error: unable to open file %s\n", argv[1]);
+    return EXIT_FAILURE;
+  }
+
+  // File length
+  int lines = 0;
+  char c;
+  char line[MAX_LINE_LENGTH];
+
+  char crossword[MAX_LINE_LENGTH][MAX_LINE_LENGTH];
+
+  // while (!feof(f)) {
+  //   while ((c = fgetc(f) != '\n')) {
+  //     printf("%c", c);
+  //   }
+  //   printf("\n");
+  // }
+
+  // Iterate through each line of the file
+  int i = 0;
+  while (fgets(line, sizeof(line), f)) {
+    // Iterate through each character of the line
+    int j = 0;
+    while (line[j] != '\0' && line[j] != '\n') {
+      crossword[i][j] = line[j];
+      j++;
+    }
+    i++;
+  }
+
+  // Go the beginning of the file
+  // fseek(listf, 0, SEEK_SET);
+
+  int matches = 0;
+
+  for (int i = 0; i < MAX_LINE_LENGTH; i++) {
+    if (crossword[i][0] == '\0') {
+      break;
+    }
+    for (int j = 0; j < MAX_LINE_LENGTH; j++) {
+      if (crossword[i][j] == '\0') {
+        break;
+      }
+
+      if (crossword[i][j] == 'A') {
+        if (crossword[i - 1][j - 1] == 'M' && crossword[i - 1][j + 1] == 'S' &&
+            crossword[i + 1][j - 1] == 'M' && crossword[i + 1][j + 1] == 'S') {
+          matches++;
+        }
+        if (crossword[i - 1][j - 1] == 'S' && crossword[i - 1][j + 1] == 'M' &&
+            crossword[i + 1][j - 1] == 'S' && crossword[i + 1][j + 1] == 'M') {
+          matches++;
+        }
+        if (crossword[i - 1][j - 1] == 'S' && crossword[i - 1][j + 1] == 'S' &&
+            crossword[i + 1][j - 1] == 'M' && crossword[i + 1][j + 1] == 'M') {
+          matches++;
+        }
+        if (crossword[i - 1][j - 1] == 'M' && crossword[i - 1][j + 1] == 'M' &&
+            crossword[i + 1][j - 1] == 'S' && crossword[i + 1][j + 1] == 'S') {
+          matches++;
+        }
+      }
+    }
+  }
+
+  // Close file
+  fclose(f);
+
+  printf("Matches: %d\n", matches);
+
+  return EXIT_SUCCESS;
+}
